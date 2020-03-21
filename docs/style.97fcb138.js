b9cb953a1feebabda7c4c57e1043dfe894c5528e
Kevin Scott <kevin@hitheory.com>
2020-03-21 10:57:42 -0400

    The public URL needs to be frontend, not docs

--- docs/style.97fcb138.js
+++ docs/style.97fcb138.js
@@ -217,7 +217,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52673" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53475" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +394,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=docs/style.97fcb138.js.map
+//# sourceMappingURL=frontend/style.97fcb138.js.map
