bdbece5fd83ccd080b46920d21315cd1a02962e0
Kevin Scott <kevin@hitheory.com>
2020-03-21 11:00:34 -0400

    Try to use an absolute public path

--- docs/src.e31bb0bc.js
+++ docs/src.e31bb0bc.js
@@ -151,7 +151,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53475" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53831" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -328,4 +328,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["../node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=frontend/src.e31bb0bc.js.map
+//# sourceMappingURL=/frontend/src.e31bb0bc.js.map
