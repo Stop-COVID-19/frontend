78b312e87e86ae8109f64bb90cf35badf5e224f5
Kevin Scott <kevin@hitheory.com>
2020-03-24 11:46:51 -0400

    Console log out the markdown

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 const markdown = require('markdown').markdown;
 const content = require('./index.md');
+console.log('the content, required from markdown', content);
 document.getElementById('content').innerHTML = markdown.toHTML(content);
