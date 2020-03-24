82e918a34f2a5df39c900b84723e9997da1bbf3f
Kevin Scott <kevin@hitheory.com>
2020-03-24 11:49:31 -0400

    Additional logging

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const markdown = require('markdown').markdown;
 const content = require('./index.md');
 console.log('the content, required from markdown', content);
-document.getElementById('content').innerHTML = markdown.toHTML(content);
+const convertedContent = markdown.toHTML(content);
+console.log('the content, converted from markdown', convertedContent);
+document.getElementById('content').innerHTML = convertedContent;
